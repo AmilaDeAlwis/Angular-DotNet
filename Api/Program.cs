34bd399859fc8b06d72a96fb3766e00dcf490221
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-02-12 20:40:22 +0530

    default connection added

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,10 @@
 using Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlite());
+builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
